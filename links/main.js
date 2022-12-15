1ee98dbec87e6d654a52ff6652e8fbb7b5025101
Minegod2012 <107361060+Minegod2012@users.noreply.github.com>
2022-12-15 19:51:27 +0530

    Delete links directory

--- links/main.js
+++
@@ -1,8 +0,0 @@
-function Function() 
-{
-    var copyText = document.getElementById("Input");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied the text: " + copyText.value);
-}
